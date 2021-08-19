c043aa78ba142aba88ef067ee52691a4e6a4bde8
Vlad Frangu <kingdgrizzle@gmail.com>
2021-08-19 14:03:42 +0200

    chore: moar prettier rules

--- javascript/packages/styleguide-prettier/index.js
+++ javascript/packages/styleguide-prettier/index.js
@@ -1,5 +1,19 @@
 module.exports = {
-    printWidth: 100,
+	endOfLine: "lf",
+    printWidth: 120,
+	quoteProps: "as-needed",
+	semi: true,
     singleQuote: false,
+	tabWidth: 4,
+	trailingComma: "all",
     useTabs: true,
+	overrides: [
+		{
+			files: "*.yml",
+			options: {
+				tabWidth: 2,
+				useTabs: false,
+			},
+		},
+	],
 };
