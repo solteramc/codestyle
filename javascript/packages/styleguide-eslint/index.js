196e2bacdbb3f588e7536eb1e1f900c45ecbdf86
SkyezerFox <actuallyori@gmail.com>
2021-08-19 12:47:25 +0100

    create ESLint and Prettier packages

---
+++ javascript/packages/styleguide-eslint/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint", "prettier"],
+    extends: ["airbnb", "prettier", "plugin:@typescript-eslint/recommended"],
+    rules: {
+        "prettier/prettier": ["error"],
+    },
+};
